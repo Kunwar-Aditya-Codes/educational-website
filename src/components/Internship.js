0e4591b0023aaa7c1e08483a4bc70d428933c6d1
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-02-10 14:23:53 +0530

    commit

--- src/components/Internship.js
+++ src/components/Internship.js
@@ -14,37 +14,44 @@ const Internship = () => {
           title={"Develop Your Skillset"}
           time={"3"}
           p1={"Online Skill Development Programs"}
+          p2={"Mr. Rajesh Sharma"}
         />
         <InternshipCard
           title={"Business Development Intern"}
           time={"3-4"}
           p1={"Finbricks"}
+          p2={"Mr. Rajesh Sharma"}
         />
         <InternshipCard title={"Data Entry Operator"} time={"4"} p1={"BWA"} />
         <InternshipCard
           title={"Lead Generation Intern"}
           time={"2"}
           p1={"Intugine Technologies"}
+          p2={"Mr. Rajesh Sharma"}
         />
         <InternshipCard
           title={"Devops"}
           time={"1-2"}
           p1={"Beagle Cyber Innovations Private Limited"}
+          p2={"Mr. Rajesh Sharma"}
         />
         <InternshipCard
           title={"Content Writing Internship"}
           time={"5"}
           p1={"lifeisoutside.com"}
+          p2={"Mr. Rajesh Sharma"}
         />
         <InternshipCard
           title={"Business Consultant"}
           time={"2-4"}
           p1={"Rosefield Energy Tech Pvt Ltd"}
+          p2={"Mr. Rajesh Sharma"}
         />
         <InternshipCard
           title={"Graphic Design Intern"}
           time={"1"}
           p1={"DIBC"}
+          p2={"Mr. Rajesh Sharma"}
         />
       </div>
     </div>
--- src/components/InternshipCard.js
+++ src/components/InternshipCard.js
@@ -1,18 +1,14 @@
 import React from "react";
 
-const InternshipCard = ({ title, p1, time }) => {
+const InternshipCard = ({ title, p1, time, p2 }) => {
   return (
-    <div className="border text-justify rounded-md p-4 w-[95%] mx-auto my-2 flex justify-between">
-      <div className=" w-2/3">
-        <h1 className="my-1 bg-white text-black rounded-md p-2 text-xl font-medium underline underline-offset-2 ">
-          {title}
-        </h1>
-        <p className="mt-3 text-lg text-slate-300">{p1}</p>
-        <p className="my-1 text-lg">Time : {time} months</p>
-      </div>
-      <button className="text-xl font-medium bg-gradient-to-r hover:scale-110 transition ease-linear duration-200 from-[#FA7268] to-[#C62368]  px-3 rounded-md ">
-        Apply
-      </button>
+    <div className="border text-center rounded-md p-4 w-[95%] mx-auto my-2">
+      <h1 className="my-1 bg-white text-black rounded-md p-2 text-xl font-medium underline underline-offset-2 ">
+        {title}
+      </h1>
+      <p className="mt-3 text-lg text-slate-300">{p1}</p>
+      <p className="mt-3 text-lg text-slate-300">Taught by - {p2}</p>
+      <p className="my-1 text-lg">Time : {time} months</p>
     </div>
   );
 };
